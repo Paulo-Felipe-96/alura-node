da4dbd989535453edb5edf1d301e158e8da0ad86
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-06-13 16:10:02 -0300

    refact: native http removed and add express server wih import export

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const http = require("http");
-const port = 3000;
+import app from "./src/app.js";
+const port = process.env.PORT || 3000;
 
-const routes = {
-  "/": "Curso de Node",
-  "/livros": "Entrei na pagina de livros",
-  "/autores": "Listagem de autores",
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(routes[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Listening on port http://localhost:${port}`);
 });
