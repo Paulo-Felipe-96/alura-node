3c2abe7750fbe69a89b8f99b4e3b68d1a36a9684
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-06-14 18:10:47 -0300

    feat: final api without db connection

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ app.use(express.json());
 const livros = [
   { id: 1, titulo: "Senhor dos Aneis" },
   { id: 2, titulo: "O Hobbit" },
+  { id: 3, titulo: "O pequeno príncipe" },
+  { id: 4, titulo: "O banana" },
 ];
 
 app.get("/", (req, res) => {
@@ -15,6 +17,11 @@ app.get("/", (req, res) => {
 
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const posicao = buscaLivro(req.params.id);
+  res.json(livros[posicao]);
 });
 
 app.post("/livros", (req, res) => {
@@ -26,6 +33,17 @@ app.put("/livros/:id", (req, res) => {
   let posicao = buscaLivro(req.params.id);
   livros[posicao].titulo = req.body.titulo;
   res.json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const posicao = buscaLivro(req.params.id);
+  if (livros.length) {
+    livros.splice(posicao, 1);
+    res.json(livros);
+  }
+  if (!livros.length) {
+    res.status(404).end(`Nenhum registro encontrado`);
+  }
 });
 
 function buscaLivro(id) {
