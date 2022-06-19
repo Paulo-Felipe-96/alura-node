768d1ddc617f776ed02cd02ec4003bbaa48b2fe1
Paulo Martins <paulo.martins@idwall.co>
2022-06-19 19:24:38 -0300

    refact: data association between autor and livros

--- src/models/Book.js
+++ src/models/Book.js
@@ -5,7 +5,11 @@ const { Schema, model } = mongoose;
 const bookSchema = new Schema({
   id: { type: String },
   titulo: { type: String, required: true },
-  autor: { type: String, required: true },
+  autor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "autores",
+    required: true,
+  },
   editora: { type: String, required: true },
   numeroPaginas: { type: Number },
 });
