ef98028ba6b44b48cfad66c96f456958425747b0
Paulo Martins <paulo.martins@idwall.co>
2022-07-07 17:46:43 -0300

    feat: add endpoints repo to welcome-page

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,11 @@ import publishers from "./publisherRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
-    res.status(200)
-      .send(`Welcome to bookstore API. See the endpoints in this repo:
-    https://github.com/Paulo-Felipe-96/bookstore-api/tree/main/__endpoints__`);
+    res.status(200).json({
+      status: 200,
+      message: `Welcome to bookstore API. See the endpoints in this repo:
+      https://github.com/Paulo-Felipe-96/bookstore-api/tree/main/__endpoints__`,
+    });
   });
 
   app.use(express.json(), books, authors, publishers);
