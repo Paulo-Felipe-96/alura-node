08b6e5a3f99f72275df61296b638c45e6adec3ba
Paulo Martins <paulo.martins@idwall.co>
2022-09-28 10:57:59 -0300

    refact: linter fix

--- src/services/dbConnect.js
+++ src/services/dbConnect.js
@@ -7,7 +7,7 @@ const { connect, connection } = mongoose;
 async function connectToDb() {
   try {
     return await connect(
-      `mongodb+srv://${mongoDbUserName}:${mongoDbPassword}@alura.dno2mge.mongodb.net/alura-node`
+      `mongodb+srv://${mongoDbUserName}:${mongoDbPassword}@alura.dno2mge.mongodb.net/alura-node`,
     ).then(connection.once("open", () => console.log("MongoDB: Connected!")));
   } catch (e) {
     handleError(e);
