9dee0d333a1dfb79aa7dd1bf2426070d33ec9605
Paulo Martins <paulo.martins@idwall.co>
2022-10-06 18:01:19 -0300

    refact: does change TryCatch place inside startup method

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@ const app = require("./app");
 const { port } = require("./config");
 const handleError = require("./helpers/handleError");
 
-function startApp() {
-  return app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+async function server(port) {
+  try {
+    return app.listen(port, () => console.log(`Server is now running on: 'http://localhost:${port}'`));
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-try {
-  startApp();
-} catch (error) {
-  handleError(error);
-}
+server(port);
