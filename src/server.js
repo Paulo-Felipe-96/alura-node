f0ea5a79a1036a40cd6e01d3d2265d15f69f1de2
Paulo Martins <paulo.martins@idwall.co>
2022-11-11 11:54:47 -0300

    fix: does fix throw error ref

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const app = require("./app");
 const { port } = require("./config");
-const handleError = require("./helpers/handleError");
+const throwError = require("./helpers/throwError");
 
 async function server(port) {
   try {
     return app.listen(port, () => console.log(`Server is now running on: 'http://localhost:${port}'`));
   } catch (error) {
-    handleError(error);
+    throwError(error);
   }
 }
 
