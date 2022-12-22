7889505c49e8a1112a9542098163d5593872a0b5
Paulo Martins <paulo.martins@idwall.co>
2022-12-22 14:48:54 -0300

    fix: does update route and test cases with auth

--- src/routes/__tests__/unit/FailTestCases.spec.js
+++ src/routes/__tests__/unit/FailTestCases.spec.js
@@ -1,12 +1,18 @@
 const request = require("supertest");
 const app = require("../../../app");
-const { badScenariosForDelete, badScenariosForPost } = require("../mocks/FailTestCasesMock");
+const {
+  badScenariosForDelete,
+  badScenariosForPost,
+} = require("../mocks/FailTestCasesMock");
 
 describe("Router test cases for fails in Livros, Editoras and Autores", () => {
   test.each(badScenariosForDelete)("$title", async ({ path }) => {
     await request(app)
       .delete(path)
-      .set("Accept", "application/json")
+      .set({
+        Accept: "application/json",
+        Authorization: "9e788a-9228e934cbb2-af876b2694e591",
+      })
       .expect("Content-Type", "text/html; charset=utf-8")
       .expect(404);
   });
@@ -14,7 +20,10 @@ describe("Router test cases for fails in Livros, Editoras and Autores", () => {
   test.each(badScenariosForPost)("$title", async ({ path }) => {
     await request(app)
       .post(path)
-      .set("Accept", "application/json")
+      .set({
+        Accept: "application/json",
+        Authorization: "9e788a-9228e934cbb2-af876b2694e591",
+      })
       .expect("Content-Type", "application/json; charset=utf-8")
       .expect(500);
   });
--- src/routes/__tests__/unit/SuccessTestCases.spec.js
+++ src/routes/__tests__/unit/SuccessTestCases.spec.js
@@ -1,18 +1,16 @@
 const request = require("supertest");
 const app = require("../../../app");
-const {
-  succeededScenariosForGet,
-} = require("../mocks/SuccessTestCasesMock");
+const { succeededScenariosForGet } = require("../mocks/SuccessTestCasesMock");
 
 describe("Well succeeded Router test cases for Livros, Editoras and Autores", () => {
-  test.each(succeededScenariosForGet)(
-    "$title",
-    async ({ path }) => {
-      await request(app)
-        .get(path)
-        .set("Accept", "application/json")
-        .expect("Content-Type", "application/json; charset=utf-8")
-        .expect(200);
-    },
-  );
+  test.each(succeededScenariosForGet)("$title", async ({ path }) => {
+    await request(app)
+      .get(path)
+      .set({
+        Accept: "application/json",
+        Authorization: "9e788a-9228e934cbb2-af876b2694e591",
+      })
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .expect(200);
+  });
 });
